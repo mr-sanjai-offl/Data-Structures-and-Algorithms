4ab237a5a3fb840aabbf2da156bd55b91096f102
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-13 23:34:27 +0530

    feat: Add Two Sum

---
+++ Array/Two Sum/python/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = {} #dictionary hash table
+        for i in range(len(nums)):
+            remain = target - nums[i] #find the remainder
+            if remain in res:
+                return res[remain], i #return the result if in the map
+            res[nums[i]]·‌=·‌i·‌#add·‌to·‌map
