e1f34c98fed9518952e344ce6f1edc00d90a3439
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-13 23:38:44 +0530

    Delete General directory

--- General/Two Sum/python/solution.py
+++
@@ -1,9 +0,0 @@
-1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        
-4        ans = {}
-5        for i in range(len(nums)):
-6            remain = target - nums[i]
-7            if remain in ans:
-8                return ans[remain], i
-9            ans[nums[i]] = i
