1f67aefb31b4430e17c0231a183396deb6b2ca66
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-13 21:06:57 +0530

    feat: Add Two Sum

--- Array/Two Sum/python/solution.py
+++ Array/Two Sum/python/solution.py
@@ -1,9 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+
+        # for i in range(0,n):
+        #     for j in range(i+1,n):
+        #         if(nums[i]+nums[j]==target):
+        #             return [i,j]
+
+        #Hash Table O(n)
+        numMap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in numMap:
+                return[numMap[complement], i]
+            numMap[num] = i
+
+        # numMap={}
+
+        # for i in range(0,len(nums)):
+        #     temp = target - nums[i]
+        #     if temp in numMap:
+        #         return [numMap[temp], i]
+        #     numMap[nums[i]] = i
+            
+
         
-        ans = {}
-        for i in range(len(nums)):
-            remain = target - nums[i]
-            if remain in ans:
-                return ans[remain], i
-            ans[nums[i]] = i
