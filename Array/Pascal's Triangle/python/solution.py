48c4842422056b33e3e45d97c65ec78695745814
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-13 19:59:26 +0530

    feat: Add Pascal's Triangle

---
+++ Array/Pascal's Triangle/python/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        res = []
+        prow = []
+        crow = []
+
+        for i in range(0,numRows):
+            for j in range(0,i+1):
+                if j==0 or j==i:
+                    crow.append(1)
+                else:
+                    temp = prow[j-1] + prow[j]
+                    crow.append(temp)
+            res.append(crow)
+            prow=crow
+            crow=[]
+
+        return res
