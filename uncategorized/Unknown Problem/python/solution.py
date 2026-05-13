1eaea2ef118206e3d302479ea140a84106e90429
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-13 21:05:44 +0530

    Delete uncategorized directory

--- uncategorized/Unknown Problem/python/solution.py
+++
@@ -1,6 +0,0 @@
-1class Solution:
-2    def reverse(self, x: int) -> int:
-3        y = int(str(abs(x))[::-1])
-4        res = -y if x < 0 else y
-5        return 0 if res < -(2**31) or res > 2**31 - 1 else res
-6
