b49e92a0ab0ce50ab93587e6ff980cf8060cc469
Sanjai S <148669925+mr-sanjai-offl@users.noreply.github.com>
2026-05-20 17:29:07 +0530

    feat: Add Customer Who Visited but Did Not Make Any Transactions

--- Database/Customer Who Visited but Did Not Make Any Transactions/sql/solution.sql
+++ Database/Customer Who Visited but Did Not Make Any Transactions/sql/solution.sql
@@ -2,5 +2,5 @@ SELECT V.customer_id, count(V.visit_id) AS count_no_trans
 FROM Visits V
 LEFT JOIN Transactions T
 ON V.visit_id = T.visit_id
-WHERE T.amount IS NULL
+WHERE T.transaction_id IS NULL
 GROUP BY V.customer_id;
