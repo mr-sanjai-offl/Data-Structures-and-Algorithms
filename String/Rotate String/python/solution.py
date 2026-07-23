b473bfdee99513f624e3224acc9a5a9fbff8a473
Sanjai S <mr.sanjai.offl@gmail.com>
2026-07-23 22:11:44 +0530

    Delete String/Rotate String directory

--- String/Rotate String/python/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        n = len(s)
-        if n != len(goal): return False
-        for i in range(1,n+1):
-            if(goal == s[i:n] + s[0:i]): return True
-        return False
