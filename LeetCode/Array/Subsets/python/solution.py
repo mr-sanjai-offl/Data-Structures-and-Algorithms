73cafb46e4ce85c7cc144151899f9543bac901e1
Sanjai S <mr.sanjai.offl@gmail.com>
2026-08-04 00:12:04 +0530

    feat: Add Subsets

--- LeetCode/Array/Subsets/python/solution.py
+++ LeetCode/Array/Subsets/python/solution.py
@@ -1,28 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def backtrack(index, current):
-            if index == len(nums):
-                result.append(current[:])
-             
-                return
-
-            # Pick
-            current.append(nums[index])
-          
-            
-            backtrack(index + 1, current)
-
-            # Backtrack
-            current.pop()
-            
-            
-
-            # Not Pick
-            backtrack(index + 1, current)
-           
-
-        backtrack(0, [])
-        return result
-        
+        n = len(nums)
+        ss = 1<<n
+        res = []
+        for num in range(0,ss):
+            sublist = []
+            for i in range(0,n):
+                if num & (1 << i):
+                    sublist.append(nums[i-1])
+            res.append(sublist)
+        return res
