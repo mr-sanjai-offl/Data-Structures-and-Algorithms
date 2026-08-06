2eeeac7f58eb5a955b095577db2f103c31e0689a
Sanjai S <mr.sanjai.offl@gmail.com>
2026-08-06 23:35:22 +0530

    feat: Add Climbing Stairs

--- LeetCode/Math/Climbing Stairs/python/solution.py
+++ LeetCode/Math/Climbing Stairs/python/solution.py
@@ -1,9 +1,22 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        i = 0
-        j = 1
-        for _ in range(n):
-            c = i+j
-            i, j = j, c
-  
-        return j
+        if n==1 or n==2:
+            return n
+        # step1 = 1
+        # step2 = 2
+        # i=3
+        # while(i<=n):
+        #     ways = step1 + step2
+        #     step1 = step2
+        #     step2 = ways
+        #     i+=1
+        # return ways
+    
+        step = [1]*n
+        step[0] = 1
+        step[1] = 2
+        for i in range(2,n):
+            step[i] = step[i-1]+step[i-2]
+        return step[-1]
+
+        
