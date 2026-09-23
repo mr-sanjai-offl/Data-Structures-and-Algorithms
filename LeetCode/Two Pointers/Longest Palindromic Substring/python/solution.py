1a61a8faa48bab14268648ff8bfe6eee5354019c
Sanjai S <mr.sanjai.offl@gmail.com>
2026-09-23 23:56:05 +0530

    feat: Add Longest Palindromic Substring

--- LeetCode/Two Pointers/Longest Palindromic Substring/python/solution.py
+++ LeetCode/Two Pointers/Longest Palindromic Substring/python/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def ispali(s):
-            return s == s[::-1]
         n = len(s)
-        if n == 1:
-            return s
-        maxlen = 0
         res = ""
-        for i in range(0,n):
+        max_len = 0
+        for i in range(n):
             for j in range(i+1,n+1):
-                sub = s[i:j]
-                if ispali(sub) and len(sub) > maxlen:
-                    maxlen = len(sub)
-                    res = sub
+                st = s[i:j]
+                if st == st[::-1] and len(st) > max_len:
+                    max_len = len(st)
+                    res = st
         return res
